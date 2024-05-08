f2b8fcf0f2f53ea8b06572d9d79148bc51a8f9de
o-aguirre <two.aguirre@gmail.com>
2024-05-08 17:01:25 -0400

    Día 1 reto

---
+++ ejercicio_1.py
@@ -0,0 +1,6 @@
+# Sumar 2 números y mostrar su resultado
+
+num1 = int(input('Ingrese un número: '))
+num2 = int(input('Ingrese un número: '))
+
+print('La suma de ambos números es: ', num1 + num2)
---
+++ ejercicio_2.py
@@ -0,0 +1,6 @@
+# Calcula el área de un círculo con un radio dado
+import math
+
+radio = int(input('Ingresa el radio del círculo: '))
+
+print('El área del círculo es de: ', (radio ** 2) * math.pi)
