93217b2e0ca2dbd4a31c5248779083649e8d58b0
o-aguirre <two.aguirre@gmail.com>
2024-05-10 13:28:41 -0400

    2024.05.10 rename files

--- ejercicio_010.py
+++ ejercicio_010.py
@@ -6,3 +6,8 @@ largo_txt = len(txt) - 1
 for i in txt:
     print(txt[largo_txt], end='')
     largo_txt -= 1
+
+# 2da forma de lograrlo
+txt_2 = input('Ingresa un texto: ')
+inverted_txt = txt_2[::-1]
+print(inverted_txt)
